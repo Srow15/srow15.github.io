5a795d154d9e752471c1b10e8cdf137d5a0cc43b
Srow15 <ben16060@byui.edu>
2019-10-20 00:57:38 -0600

    update

--- Lesson5/js/showBanner.js
+++ Lesson5/js/showBanner.js
@@ -8,7 +8,7 @@ function showBanner() {
         document.getElementById("banner").innerHTML = "Saturday = Preston Pancakes in the Park!  9:00 a.m. Saturday at the city park pavilion."
         
     } else {
-        document.getElementById("banner").style.display = none;
+        document.getElementById("banner").style.display = block;
     }
     
 }
