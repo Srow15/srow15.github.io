61285b39e54b55f3776c327a17ae5882787f6f91
Srow15 <ben16060@byui.edu>
2019-10-26 17:34:50 -0600

    update

--- lesson6/js/windchill.js
+++ lesson6/js/windchill.js
@@ -10,7 +10,7 @@ function newChill(){
     
     
     
-        function windChill(temp, speed){
+        function windchill(temp, speed){
             var f = 'sarah';
             if (temp <= 50 && speed > 3) {  
                 f = 32.74 + (0.6215 * temp)-(35.75 * Math.pow(speed,0.16)) + (0.4275 * temp * Math.pow(speed,0.16));
