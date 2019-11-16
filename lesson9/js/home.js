e28ffcdc78ee87c42ce56864ece358d2483d5a27
Srow15 <ben16060@byui.edu>
2019-11-15 22:10:49 -0700

    update

--- lesson9/js/home.js
+++ lesson9/js/home.js
@@ -7,8 +7,8 @@ fetch(requestURL)
         //console.table(jsonObject);  // temporary checking for valid response and data parsing
         const towns = jsonObject['towns'];
         for (let i = 0; i < towns.length; i++) {
-            let card = document.createElement('section');
 
+            let card = document.createElement('section');
             let h2 = document.createElement('h2');
             let h3 = document.createElement('h3');
             let founded = document.createElement('p');
@@ -16,15 +16,16 @@ fetch(requestURL)
             let rainfall = document.createElement('p');
             let img = document.createElement('img');
 
-            h2.textContent = towns[i].name;
+            if (towns[i].name == "Fish Haven" || towns[i].name == "Soda Springs" || towns[i].name == "Preston")
+
+            {h2.textContent = towns[i].name;
             h3.textContent = towns[i].motto;
             founded.textContent = 'Year Founded:' + ' ' + towns[i].yearFounded;
             population.textContent = 'Population:' + ' ' + towns[i].currentPopulation;
             rainfall.textContent = 'Annual Rain Fall:' + ' ' + towns[i].averageRainfall;
             img.setAttribute('src', towns[i].photo);
             img.setAttribute('alt', towns[i].name);
-
-
+            }
             card.appendChild(h2);
             card.appendChild(h3);
             card.appendChild(founded);
@@ -36,5 +37,5 @@ fetch(requestURL)
 
         }
 
-    });
 
+    });
