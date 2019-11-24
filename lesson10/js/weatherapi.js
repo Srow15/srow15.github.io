53c51a753482583b5a7037ba6bff64d1d8e8e989
Srow15 <ben16060@byui.edu>
2019-11-23 21:38:22 -0700

    update

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -17,54 +17,53 @@ switch (page) {
         console.log("Error")
         break;
 }
-    
-    const weatherAPI = "https://api.openweathermap.org/data/2.5/weather?id=" + townID + "&units=imperial&APPID=ded5590f5d505bff36ebae4bc57bc495";
-    fetch(weatherAPI)
-        .then((response) => response.json())
-        .then((jsObject) => {
-      //console.log(jsObject);
-  
-      document.getElementById('weather').textContent = jsObject.id.main;
-      document.getElementById('high').textContent = jsObject.main.temp;
-      document.getElementById('humidity').textContent = jsObject.main.humidity;
-      document.getElementById('speed').textContent = jsObject.wind.speed;
-  
-      const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';  // note the concatenation
-      const desc = jsObject.weather[0].description;  // note how we reference the weather array
-      document.getElementById('weather').textContent = desc;
-  
+
+const weatherAPI = "https://api.openweathermap.org/data/2.5/weather?id=" + townID + "&units=imperial&APPID=ded5590f5d505bff36ebae4bc57bc495";
+fetch(weatherAPI)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //console.log(jsObject);
+
+        document.getElementById('weather').textContent = jsObject.id.main;
+        document.getElementById('high').textContent = jsObject.main.temp;
+        document.getElementById('humidity').textContent = jsObject.main.humidity;
+        document.getElementById('speed').textContent = jsObject.wind.speed;
+
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+        document.getElementById('weather').textContent = desc;
+
     });
 
-    const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?id=" + townID + "&units=imperial&APPID=ded5590f5d505bff36ebae4bc57bc495";
-    fetch(forecastAPI)
-        .then((response) => response.json())
-        .then((jsObject) => {
-    //console.log(jsObject);
+const forecastAPI = "https://api.openweathermap.org/data/2.5/forecast?id=" + townID + "&units=imperial&APPID=ded5590f5d505bff36ebae4bc57bc495";
+fetch(forecastAPI)
+    .then((response) => response.json())
+    .then((jsObject) => {
+        //console.log(jsObject);
 
-    let forcastinfo = document.getElementsById('day' + count);
-    let forcastday = document.getElementsById('day' + count);
-    let x = 0
-    for (let i = 0; i < jsObject.list.length; i++) {
-      if (jsObject.list[i].dt_txt.includes('18:00:00')) {
-        //console.log(jsObject.list[i].dt_txt);
 
-                
+        let count = 0
+        let info = document.getElementById('info' + count);
+        let name = document.getElementById('day' + count);
+        for (i = 0; i < jsObject.list.length; i++) {
+            if (jsObject.list[i].dt_txt.includes('18:00:00')) {
+
                 let text = document.createElement('p')
-                    text.innerHTML = jsObject.list[i].main.temp + " &#176;F";
-                
+                text.innerHTML = jsObject.list[i].main.temp + " &#176;F";
+
                 let icon = jsObject.list[i].weather[0].icon;
                 let image = document.createElement('img');
                 let desc = jsObject.list[i].weather[0].description
                 image.setAttribute('src', "https://openweathermap.org/img/wn/" + icon + ".png");
                 image.setAttribute('alt', desc);
 
-                forcastinfo[x].appendChild(image);
-                forcastinfo[x].appendChild(text);
+                info[i].appendChild(image);
+                info[i].appendChild(text);
 
-                let day = new Date(jsObject.list[i].dt_txt.;
+                let day = new Date(jsObject.list[i].dt_txt.substring(0, 11));
                 day = day.getDay();
-                forcastday[x].appendChild(day);        
-
-     }
-    }
-    });
+                name[i].innerHTML = daynames[day];
+                count++
+            }
+        }
+    });;
