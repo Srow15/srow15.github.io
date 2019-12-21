99eefbcb4c022f17ca229081f41860cdea71c981
Srow15 <ben16060@byui.edu>
2019-12-20 17:54:49 -0700

    update

--- finalesite/js/temple.js
+++ finalesite/js/temple.js
@@ -9,24 +9,35 @@ fetch(requestURL)
     for (let i = 0; i < temples.length; i++ ) {
         let card = document.createElement('section');
 let h2 = document.createElement('h2');
+let h31 = document.createElement('h3');
+let h32 = document.createElement('h3');
+let h33 = document.createElement('h3');
 let p1 = document.createElement('p');
 let p2 = document.createElement('p');
 let p3 = document.createElement('p');
-let ul = document.createElement('ul');
+let p4 = document.createElement('p');
 let img = document.createElement('img');
 
 h2.textContent = temples[i].name;
+h31.textContent = 'Address';
 p1.textContent = temples[i].address;
+h32.textContent = 'Telephone';
 p2.textContent = temples[i].telephone;
+h33.textContent = 'Email';
 p3.textContent = temples[i].email;
-ul.textContent = temples[i].services;
+p4.textContent = temples[i].services;
 img.setAttribute('src', temples[i].imageurl);
 img.setAttribute('alt', temples[i].name);
 
 card.appendChild(h2);
 card.appendChild(img);
+card.appendChild(h31);
 card.appendChild(p1);
+card.appendChild(h32);
 card.appendChild(p2);
+card.appendChild(h33);
+card.appendChild(p3);
+card.appendChild(p4);
 
 document.querySelector('div.cards').appendChild(card);
 
